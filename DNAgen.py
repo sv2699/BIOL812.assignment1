46ca81ef7034400b01a61d671db623fdd77651ee
sv2699 <103062241+sv2699@users.noreply.github.com>
2022-04-08 10:40:54 -0400

    Update DNAgen.py

--- DNAgen.py
+++ DNAgen.py
@@ -5,7 +5,7 @@ random.seed (123)
 def generateFASTA(filename, Nb):
     filename = input ("Enter filename:")
     Nb = input ("Enter number of base pairs:")
-    dnafile = open(filename.seq, "w+" )
-    return ''.join(random.choice('CGTA') for _ in range(Nb))
+    dnafile = open(inputfilename.seq, "w+" )
+    return ''.join(random.choice('CGTA') for _ in range(inputNb))
     dnafile.close()
     
